f7bb141113b0e843cb6632059d44b4429aca4e45
chthomps <67965487+chthomps@users.noreply.github.com>
2020-07-30 00:19:27 -0500

    Update Figures.java

--- src/Figures.java
+++ src/Figures.java
@@ -13,41 +13,47 @@ public class Figures {
 	public static void main(String[] args) {
 
 		//Prompting user to input figure size (length x width)
-		System.out.println("Please enter an odd integer defining the size (length x width) of the figure");
+		System.out.print("Please enter an odd integer defining the size (length x width) of the figure");
 		
 		Scanner scan = new Scanner(System.in);
 		
 		size = scan.nextInt();
-		
-		//Presents menu to user 
-		System.out.println("MENU: \n1. Print Box \n2. Print Diamond \n3. Print X \n4. Quit Program ");
-		
-		selection = scan.nextInt();
 
 		do {
+			
+			
+			//Presents menu to user 
+			System.out.println("MENU: \n1. Print Box \n2. Print Diamond \n3. Print X \n4. Quit Program \n");
+			System.out.print("Please select an option: ");
+			
+			selection = scan.nextInt();
+			
 			switch (selection) {
 			case 1:
 				Figures Box = new Figures();
 				Box.size = size;
 				Box.printBox();
+				System.out.println();
 				break;
 			case 2:
 				Figures Diamond = new Figures();
 				Diamond.size = size;
 				Diamond.printDiamond();
+				System.out.println();
 				break;
 			case 3:
 				Figures X = new Figures();
 				X.size = size;
 				X.printX();
+				System.out.println();
 				break;
 			default:
 				
-	    selection = 5;
+				
 			}
 		} while (selection<=4);	
-		scan.close();
 		
+	
 		
 	}
 
@@ -66,26 +72,26 @@ public class Figures {
 		}
 	}
 
-//Establishes the printBox method printing "*"
+//Establishes the printBox method printing "X"
 	public void printBox() {
 
 		for (int l = 0; l < size; l++) {
 			for (int w = 0; w < size; w++) {
-				System.out.print("*");
+				System.out.print("X");
 			}
 			System.out.println();
 		}
 
 	}
 	
-//Establishes the printDiamond method printing "*"
+//Establishes the printDiamond method printing "X"
 	public void printDiamond() {
 		
 		for (int row = 0; row < size; row++) {
 
 			for (int col= 0; col < size; col++) {
 				if (row+col == size/2 || col ==row+(size/2) || row+col==(size-1)+(size/2) || row==(col-1)+(size/2)){
-					System.out.print("*");
+					System.out.print("X");
 				} else
 					System.out.print(" ");
 			}
