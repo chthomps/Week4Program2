e44677cb40e24f0255d0da800a2a9f71514f8d2a
chthomps <67965487+chthomps@users.noreply.github.com>
2020-08-01 07:33:01 -0500

    Update Figures.java

--- src/Figures.java
+++ src/Figures.java
@@ -7,40 +7,37 @@ public class Figures {
 
 //Constructor method
 	public void Figures(int size) {
-		
 	}
 
 //Main method
 	public static void main(String[] args) {
 
-		//Prompting user to input figure size (length x width)
+		// Prompting user to input figure size (length x width)
 		System.out.print("Please enter an odd integer defining the size (length x width) of the figure:");
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		size = scan.nextInt();
-		
 
-		if (size%2 == 0) {
-		 System.out.println("Invalid figure size- must be an odd number");
-		 System.out.println();
-		 System.out.println("Reenter the size of the figure: ");
-			
-		 size = scan.nextInt();
-			}
-		
-		if (size%2 != 0) 
-		 do {
-			
-			//Presents menu to user 
+		if (size % 2 == 0) {
+			System.out.println("Invalid figure size- must be an odd number");
 			System.out.println();
-			System.out.println("MENU: \n1. Print Box \n2. Print Diamond \n3. Print X \n4. Quit Program \n");
-			System.out.print("Please select an option: ");
-			
-			selection = scan.nextInt();
+			System.out.println("Reenter the size of the figure: ");
 
-			
-			switch (selection) {
+			size = scan.nextInt();
+		}
+
+		if (size % 2 != 0)
+			do {
+
+				// Presents menu to user
+				System.out.println();
+				System.out.println("MENU: \n1. Print Box \n2. Print Diamond \n3. Print X \n4. Quit Program \n");
+				System.out.print("Please select an option: ");
+
+				selection = scan.nextInt();
+
+				switch (selection) {
 				case 1:
 					Figures Box = new Figures();
 					Box.size = size;
@@ -62,12 +59,12 @@ public class Figures {
 					X.printX();
 					System.out.println();
 					break;
-				case 4: 
-				System.out.println("Good bye!");
-				scan.close();
-			}
-		} while (selection>=1 && selection<=4 );	
-			
+				case 4:
+					System.out.println("Good bye!");
+					scan.close();
+				}
+			} while (selection >= 1 && selection <= 4);
+
 	}
 
 //Establishes the printX method printing "X"
@@ -75,8 +72,8 @@ public class Figures {
 
 		for (int row = 0; row < size; row++) {
 
-			for (int col= 0; col < size; col++) {
-				if (col == row || col == (size - (row+1))) {
+			for (int col = 0; col < size; col++) {
+				if (col == row || col == (size - (row + 1))) {
 					System.out.print("X");
 				} else
 					System.out.print(" ");
@@ -96,21 +93,22 @@ public class Figures {
 		}
 
 	}
-	
+
 //Establishes the printDiamond method printing "X"
 	public void printDiamond() {
-		
+
 		for (int row = 0; row < size; row++) {
 
-			for (int col= 0; col < size; col++) {
-				if (row+col == size/2 || col ==row+(size/2) || row+col==(size-1)+(size/2) || row==(col-1)+(size/2)){
+			for (int col = 0; col < size; col++) {
+				if (row + col == size / 2 || col == row + (size / 2) || row + col == (size - 1) + (size / 2)
+						|| row == (col - 1) + (size / 2)) {
 					System.out.print("X");
 				} else
 					System.out.print(" ");
 			}
 			System.out.println();
-		
+
+		}
+
 	}
-	
-}
 }
